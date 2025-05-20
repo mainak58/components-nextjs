e360dcf3971bbd704ffec09642b24e3c71c15fdb
mainak58 <iammainak58@gmail.com>
2025-05-20 18:19:42 +0000

    sub small change

---
+++ src/hooks/usePaid.ts
@@ -0,0 +1,18 @@
+export default function usePaid() {
+  const paid = [
+    {
+      id: 1,
+      name: " paid Section1",
+    },
+    {
+      id: 2,
+      name: "paid Section2",
+    },
+    {
+      id: 3,
+      name: "paid Section1",
+    },
+  ];
+
+  return paid;
+}
